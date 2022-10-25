9f4b642068a35199c3f9b1c110625879bdf701ab
azizulhakims <it.azizulhakim@gmsil.com>
2022-10-25 13:16:55 +0600

    add data file

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json');
 
-app.get('/', (req, res) => {
-    res.send('Course Api Running');
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
 })
 
 app.listen(port, () => {
     console.log('Course Server running on port', port);
 })
+

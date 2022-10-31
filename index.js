746840173f8bdf7bd5d6ca7d60afe1d6bb16a980
azizulhakims <it.azizulhakim@gmsil.com>
2022-10-31 17:21:38 +0600

    add index.js file

--- index.js
+++ index.js
@@ -6,11 +6,42 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
+
+app.get('/', (req, res) => {
+    res.send('News API Running');
+})
 
 
 app.get('/course-categories', (req, res) => {
     res.send(categories);
 })
+
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(courses);
+    } else {
+        const categoryCourses = courses.filter(n => n.category_id === id);
+        res.send(categoryCourses);
+    }
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(c => c._id === id);
+    res.send(selectedCourse);
+    // console.log(req.params.id);
+})
+
+
+
 
 app.listen(port, () => {
     console.log('Course Server running on port', port);
